de659eb976935187fcf1e01afc1d48140cc81d5c
Rene Ortega <reeneortegaa@gmail.com>
2024-04-18 17:20:48 -0600

    some changes to js

--- ingreso.js
+++ ingreso.js
@@ -1,14 +1,11 @@
 
-document.querySelector('b1').addEventListener('click', function() {
-    var ingreso = document.getElementById('in').value;
-    var egreso = document.getElementById('egreso').value;
-    var concepto2 = document.getElementById('concepto2').value;
+const income = ["INGRESO: ", " "];
 
-    // Perform necessary calculations or operations with the input values
+function ingreso() {
+    const b1 = document.querySelector('b1');
 
-    //Update the disabled input fields with the calculated values
-    document.querySelector('table tr:nth-child(1) input').value = ingreso;
-    document.querySelector('table tr:nth-child(3) input').value = egreso;
-    document.querySelector('table tr:nth-child(4) input').value = concepto2;
-});
+    b1.addEventListener('click', ingreso);
+
+
+}
 
